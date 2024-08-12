03e3ece61e19554ba5b04999a71df0d2d9041fea
Aliaksandrsw <swordswird@proton.me>
2024-08-12 22:48:13 +0300

    реализация регистрации заказов

---
+++ orders/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+
+class OrderCreateView(FormView):
+    template_name = 'orders/order/create.html'
+    form_class = OrderCreateForm
+    success_url = reverse_lazy('order_created')  # Замените на URL успешного создания заказа
+
+    def form_valid(self, form):
+        cart = Cart(self.request)
+        order = form.save()
+        for item in cart:
+            OrderItem.objects.create(
+                order=order,
+                product=item['product'],
+                price=item['price'],
+                quantity=item['quantity']
+            )
+        cart.clear()
+        return render(self.request, 'orders/order/created.html', {'order': order})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart = Cart(self.request)
+        context['cart'] = cart
+        return context
